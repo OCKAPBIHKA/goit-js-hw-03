080eb77f52b0109a123ecc43d8b552bcaf0979cf
OCKAPBIHKA <152022576+OCKAPBIHKA@users.noreply.github.com>
2024-03-06 18:13:20 +0200

    make task-1, 2

---
+++ js/task-3.js
@@ -0,0 +1,7 @@
+'use strict';
+function filterArray(numbers, value) {
+  const nweFilterArray = [];
+  const doMassiv = numbers.indexOf(value);
+
+  if (doMassiv === value) return nweFilterArray.push(index);
+}
